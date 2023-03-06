481de94c5c8565d05d374c6c40ff7426c69efb9c
Mehdi Hamida <57850039+Mehdi-Hamida@users.noreply.github.com>
2023-03-06 22:51:09 +0100

    Trying to fix poorly centered display on some mobiles

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 const {scrollTop, clientHeight} = document.documentElement; // I am creating a constant from 2 of the properties of the document.documentElement object. It's called destructuring. scrollTop corresponds to the number of pixels scrolled from the top of the document. clientHeight corresponds to the height of the visible part of the client => The Viewport.
 
+
+// Function to disable negative margin-top when scrolling below #landing-section
+const stickyTitleBg = document.getElementById("sticky-title-bg");
+
+window.addEventListener("scroll", () =>
+{
+  stickyTitleBg.style.marginTop = "0";
+});
+
+
 // Function to animate the 2 cards from #presentation-section when scrolling on this section
 const presentationSection = document.getElementById("presentation-section");
 const leftCard = document.getElementById("left-card");
@@ -7,9 +17,9 @@ const rightCard = document.getElementById("right-card");
 
 window.addEventListener("scroll", () => {
   
-  const topElementToTopViewport = presentationSection.getBoundingClientRect().top; // getBoundingClientRect is an object that contains several pieces of information and we only keep the 'top' information which corresponds to the distance between the visible part of the window and the top of our element.
+  const topElementToTopViewport = presentationSection.getBoundingClientRect().top;
 
-  if (scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight) // Launches from 5vh over #presentation-section
+  if (scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight)
   {
     leftCard.style.animation = "from-left-appearing 2000ms ease";
     rightCard.style.animation = "from-right-appearing 2000ms ease";
@@ -29,9 +39,9 @@ const glassCard = document.getElementById("glass-card");
 
 window.addEventListener("scroll", () => {
   
-  const topElementToTopViewport = moreInfosSection.getBoundingClientRect().top; // getBoundingClientRect is an object that contains several pieces of information and we only keep the 'top' information which corresponds to the distance between the visible part of the window and the top of our element.
+  const topElementToTopViewport = moreInfosSection.getBoundingClientRect().top;
 
-  if (scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight) // Launches from 5vh over #presentation-section
+  if (scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight)
   {
     glassCard.classList.add("glass-card-animations");
   }
