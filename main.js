89306f8f1647bf55b2f0e03f165a9b66b0f4cde9
Mehdi Hamida <57850039+Mehdi-Hamida@users.noreply.github.com>
2023-03-07 22:18:57 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,5 @@
 const {scrollTop, clientHeight} = document.documentElement; // I am creating a constant from 2 of the properties of the document.documentElement object. It's called destructuring. scrollTop corresponds to the number of pixels scrolled from the top of the document. clientHeight corresponds to the height of the visible part of the client => The Viewport.
 
-
-// Function to disable negative margin-top when scrolling below #landing-section
-const stickyTitleBg = document.getElementById("sticky-title-bg");
-
-window.addEventListener("scroll", () =>
-{
-  stickyTitleBg.style.marginTop = "0";
-});
-
-
 // Function to animate the 2 cards from #presentation-section when scrolling on this section
 const presentationSection = document.getElementById("presentation-section");
 const leftCard = document.getElementById("left-card");
@@ -17,9 +7,9 @@ const rightCard = document.getElementById("right-card");
 
 window.addEventListener("scroll", () => {
   
-  const topElementToTopViewport = presentationSection.getBoundingClientRect().top;
+  const topElementToTopViewport = presentationSection.getBoundingClientRect().top; // getBoundingClientRect is an object that contains several pieces of information and we only keep the 'top' information which corresponds to the distance between the visible part of the window and the top of our element.
 
-  if (scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight)
+  if (scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight) // Launches from #presentation-section
   {
     leftCard.style.animation = "from-left-appearing 2000ms ease";
     rightCard.style.animation = "from-right-appearing 2000ms ease";
